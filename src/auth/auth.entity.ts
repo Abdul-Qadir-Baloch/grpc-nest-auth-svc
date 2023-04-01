b69fa34cd26cc4a1a2d33bfa5bbc8fbe5e32903d
Abdul-Qadir-Baloch <abdulqadir19974@gmail.com>
2023-04-01 08:53:05 +0500

    winston-logger-using-middleware

--- src/auth/auth.entity.ts
+++ src/auth/auth.entity.ts
@@ -1,6 +1,9 @@
 import { Exclude } from 'class-transformer';
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-
+export enum Roles {
+  User ='user',
+  Admin = 'admin'
+}
 @Entity()
 export class Auth extends BaseEntity {
   @PrimaryGeneratedColumn()
@@ -8,6 +11,11 @@ export class Auth extends BaseEntity {
 
   @Column({ type: 'varchar' })
   public email!: string;
+  @Column({type:'enum'})
+  public role :Roles ;
+
+  @Column({ type: 'varchar',nullable:true })
+  public location: string;
 
   @Exclude()
   @Column({ type: 'varchar' })
